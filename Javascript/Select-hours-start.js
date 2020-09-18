cf8308f9522f8afa593a50ae05ea42bf890ead3a
Clive Stewart <clivestewart@hotmail.com>
2020-09-18 15:32:15 -0400

    Create Select-hours-start.js

---
+++ Javascript/Select-hours-start.js
@@ -0,0 +1,98 @@
+<select class="hours-start">
+
+                        <option value="0 0.0">12:00 am (midnight)</option>
+
+                        <option value="0 0.5">12:30 am </option>
+
+                        <option value="0 1.0">1:00 am </option>
+
+                        <option value="0 1.5">1:30 am </option>
+
+                        <option value="0 2.0">2:00 am </option>
+
+                        <option value="0 2.5">2:30 am </option>
+
+                        <option value="0 3.0">3:00 am </option>
+
+                        <option value="0 3.5">3:30 am </option>
+
+                        <option value="0 4.0">4:00 am </option>
+
+                        <option value="0 4.5">4:30 am </option>
+
+                        <option value="0 5.0">5:00 am </option>
+
+                        <option value="0 5.5">5:30 am </option>
+
+                        <option value="0 6.0">6:00 am </option>
+
+                        <option value="0 6.5">6:30 am </option>
+
+                        <option value="0 7.0">7:00 am </option>
+
+                        <option value="0 7.5">7:30 am </option>
+
+                        <option value="0 8.0">8:00 am </option>
+
+                        <option value="0 8.5">8:30 am </option>
+
+                        <option value="0 9.0" selected="">9:00 am </option>
+
+                        <option value="0 9.5">9:30 am </option>
+
+                        <option value="0 10.0">10:00 am </option>
+
+                        <option value="0 10.5">10:30 am </option>
+
+                        <option value="0 11.0">11:00 am </option>
+
+                        <option value="0 11.5">11:30 am </option>
+
+                        <option value="0 12.0">12:00 pm (noon)</option>
+
+                        <option value="0 12.5">12:30 pm </option>
+
+                        <option value="0 13.0">1:00 pm </option>
+
+                        <option value="0 13.5">1:30 pm </option>
+
+                        <option value="0 14.0">2:00 pm </option>
+
+                        <option value="0 14.5">2:30 pm </option>
+
+                        <option value="0 15.0">3:00 pm </option>
+
+                        <option value="0 15.5">3:30 pm </option>
+
+                        <option value="0 16.0">4:00 pm </option>
+
+                        <option value="0 16.5">4:30 pm </option>
+
+                        <option value="0 17.0">5:00 pm </option>
+
+                        <option value="0 17.5">5:30 pm </option>
+
+                        <option value="0 18.0">6:00 pm </option>
+
+                        <option value="0 18.5">6:30 pm </option>
+
+                        <option value="0 19.0">7:00 pm </option>
+
+                        <option value="0 19.5">7:30 pm </option>
+
+                        <option value="0 20.0">8:00 pm </option>
+
+                        <option value="0 20.5">8:30 pm </option>
+
+                        <option value="0 21.0">9:00 pm </option>
+
+                        <option value="0 21.5">9:30 pm </option>
+
+                        <option value="0 22.0">10:00 pm </option>
+
+                        <option value="0 22.5">10:30 pm </option>
+
+                        <option value="0 23.0">11:00 pm </option>
+
+                        <option value="0 23.5">11:30 pm </option>
+                </select>
